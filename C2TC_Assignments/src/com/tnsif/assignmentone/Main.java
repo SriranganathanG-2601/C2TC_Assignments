cc4a8713627b10ec661cec658de2597973efa1cb
ranga <ranga@Ranga>
2025-10-06 10:21:06 +0530

    Assignment

---
+++ C2TC_Assignments/src/com/tnsif/assignmentone/Main.java
@@ -0,0 +1,11 @@
+package com.tnsif.assignmentone;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Commission cm = new Commission();
+		System.out.println(cm.toString());
+	}
+
+}
